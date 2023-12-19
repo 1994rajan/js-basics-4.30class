897e44087cadede4ca5ce478e8b9d000890dece2
rajan2054 <rajansunuwar43@gmail.com>
2023-12-19 18:06:04 +0545

    interaction

--- index.js
+++ index.js
@@ -9,12 +9,21 @@ var person = "raktim";
 let person1 = "ram";
 person1 = "raktim";
 person1 = "12352";
+person1 = "3.56";
+let person3 = "haha";
 
 // const refers to constant so it doesnt allow
 // us to change the variable
-const person2 = "hari";
+const person2 = "469885";
 //person2 = "sita";
 
 // ctrl+/ (comment)
-console.log(person);
-console.log(person1);
+console.log(person, ",", person1, person2, person3);
+
+// /*/ for multi comments
+
+//interactions
+alert("hello");
+const form1 = prompt("what is your name");
+const form = confirm("are you 18 and above ?");
+console.log(form1, form);
