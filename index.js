e6ef5a616e81ae16c925f4b16fd6e0ad9e5ae404
rajan2054 <rajansunuwar43@gmail.com>
2023-12-19 17:42:00 +0545

    foundation of variables

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("hello world");
+
+//Es5
+
+var person = "raktim";
+
+//Es6
+// let allows you to reassign the variable
+let person1 = "ram";
+person1 = "raktim";
+person1 = "12352";
+
+// const refers to constant so it doesnt allow
+// us to change the variable
+const person2 = "hari";
+//person2 = "sita";
+
+// ctrl+/ (comment)
+console.log(person);
+console.log(person1);
