b5c1cb7bb85a180453f07860c6fe4d8e3c5be431
rajan2054 <rajansunuwar43@gmail.com>
2023-12-21 18:09:51 +0545

    loop

---
+++ Loops/Loops.js
@@ -0,0 +1,20 @@
+// DRY {dont repeat yourself} => function
+// SOC {separation of concern}
+
+// write a loop that prints from 1 to 100
+
+/*
+for loop
+do while loop
+while loop
+
+*/
+
+// for (let a = 1; a <= 100; a++) {
+//   console.log(a);
+// }
+let i = 1;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
--- operations-statements/statements.js
+++ operations-statements/statements.js
@@ -10,6 +10,15 @@
 // } else {
 //   alert("you are female");
 // }
+// const result =
+//   gender === "m" ? console.log("you are male") : console.log("you are female");
+
+// write ternary operator to check if a user can vote or not (above18)
+const age = Number(prompt("what is your age ?"));
+
+// ternary operator
+const result = 
+age =>  18 ? console.log("you can vote") : console.log("you can not vote");
 
 /*
 =  a=b(value)
@@ -29,15 +38,15 @@
 //     console.log("else");
 // }
 
-const num1 = Number(prompt("enter first value"));
-const num2 = Number(prompt("enter second value"));
+// const num1 = Number(prompt("enter first value"));
+// const num2 = Number(prompt("enter second value"));
 
-// logic to check if the user has input charecter
+// // logic to check if the user has input charecter
 
-const isVaidnum1 =
-  (num1.charCodeAt() >= 65 && num1.charCodeAt() <= 90) ||
-  (num1.charCodeAt() >= 97 && num1.charCodeAt() <= 122);
+// const isVaidnum1 =
+//   (num1.charCodeAt() >= 65 && num1.charCodeAt() <= 90) ||
+//   (num1.charCodeAt() >= 97 && num1.charCodeAt() <= 122);
 
-const isVaidnum2 =
-  (num2.charCodeAt() >= 65 && num2.charCodeAt() <= 90) ||
-  (num2.charCodeAt() >= 97 && num2.charCodeAt() <= 122);
+// const isVaidnum2 =
+//   (num2.charCodeAt() >= 65 && num2.charCodeAt() <= 90) ||
+//   (num2.charCodeAt() >= 97 && num2.charCodeAt() <= 122);
