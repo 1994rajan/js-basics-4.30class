2767a5b5ba8eced48f8ac193c44761d351e31b11
rajan2054 <rajansunuwar43@gmail.com>
2023-12-21 17:25:44 +0545

    loop foundation

--- operations-statements/statements.js
+++ operations-statements/statements.js
@@ -19,12 +19,25 @@
 
 // switch - case
 
-const value = Number(prompt("enter a value"));
+// const value = Number(prompt("enter a value"));
 
-switch (value) {
-  case 1:
-    console.log("1");
-    break;
-  default:
-    console.log("else");
-}
+// switch (value) {
+//   case 1:
+//     console.log("1");
+//     break;
+//   default:
+//     console.log("else");
+// }
+
+const num1 = Number(prompt("enter first value"));
+const num2 = Number(prompt("enter second value"));
+
+// logic to check if the user has input charecter
+
+const isVaidnum1 =
+  (num1.charCodeAt() >= 65 && num1.charCodeAt() <= 90) ||
+  (num1.charCodeAt() >= 97 && num1.charCodeAt() <= 122);
+
+const isVaidnum2 =
+  (num2.charCodeAt() >= 65 && num2.charCodeAt() <= 90) ||
+  (num2.charCodeAt() >= 97 && num2.charCodeAt() <= 122);
